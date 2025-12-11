a8f317156e45e2a155df22919032680262a8b72e
Shane <sodonne8@tcd.ie>
2025-12-11 19:35:51 +0000

    Day 3 fully complete and working

--- Day_3/main.cpp
+++ Day_3/main.cpp
@@ -3,13 +3,25 @@
 
 void process();
 int getInt(char temp);
+long long ipow(long long val, int p);
 
 using namespace std;
 
 string input; 
-int sum = 0;
+long long sum = 0;
 
 // Answer: 17085
+
+void println(string input){
+    cout << input << endl;
+}
+
+void printArray( int array[], int l){
+    for(int i = 0; i < l; i++){
+        cout << array[i];
+    }
+    cout << endl;
+}
 
 int main(){
     while (true)
@@ -25,27 +37,61 @@ void process(){
     //iterate through 
     input = "";
     cin >> input;
+    const int s = 12;
+    int max[s];
 
-    int max = 0;
-    int max2 = 0;
-
+    for (int i = 0; i < s; i++){
+        max[i] = 0;
+    }
+    //987654321111111
+    //811111111111119
+    //234234234234278
     for( int i = 0; i < input.length(); i++){
         int curr = getInt(input[i]);
-        if( curr > max && i + 1 < input.length()){
-            max = curr;
-            max2 = 0;
+        
+        for( int j = 0; j < s; j++){
+            
+            if(curr > max[j] && input.length() - i >= s - j ){ //the remaining digits are enough to fill the remaining slots
+                
+                max[j] = curr;
+                for(int it = j+1; it < s; it++){
+                    max[it] = 0;
+                }
+                j = s;
+            }
+
         }
-        else if ( curr > max2)
-            max2 = curr;
+        
+      
+        
     }
 
-    cout << max << max2 << endl;
+    printArray(max,s);
+    
+    long long val = 0;
 
-    max = (max * 10) + max2;
-    cout << max << endl;
+    for(int i = 0; i < s; i++){
+        val = val + max[s -1 - i]*ipow(10,i);
+        
+        //cout << "on loop number " << i << " val is = " << val << endl;
+    }
+    sum = sum + val;
+    cout << "sum is: " << sum << endl;
+}
 
-    sum = sum + max;
-    cout << sum << endl;
+long long ipow(long long val, int p){
+    long long ans = val;
+    if( p == 0)
+        return 1;
+    else if (p == 1)
+        return val;
+    p--;
+    
+    for(int i = 0; i < p; i++){
+        ans = ans * val;
+    }
+
+    return ans;
 }
 
 int getInt(char temp){
@@ -53,3 +99,5 @@ int getInt(char temp){
     val = val - 48;
     return val;
 }
+
+// correct answer 169408143086082 lets goooo
